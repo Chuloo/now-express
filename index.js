bc49780186926fc958a71dbe56f2d4059383b893
home <home@Chuloos-Macbook-Pro.local>
2019-06-08 20:09:30 +0100

    clean clean

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
 const port = 5000;
-
-app.use(cors());
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
